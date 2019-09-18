282d4367afd8bfb526699bf944cb54061791bc77
hxf <hxf@892518100.com>
2019-09-18 21:08:04 +0800

    两个一级路由

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from './views/home/'
+import Login from './views/login'
 
 Vue.use(Router)
 
@@ -9,7 +10,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      // name: 'home',
+      redirect: '/home',
+      component: Home
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
       component: Home
     }
     // {
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>我是主页</div>
+  <div >我是主页111</div>
 </template>
 
 <script>
