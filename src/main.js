0c915a39bf44d8b1de408f9e7fef65007a24a3db
hxf <hxf@892518100.com>
2019-09-18 19:35:40 +0800

    样式修改

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUl from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUl)
 new Vue({
   router,
   render: h => h(App)
