d4042007a7b25709e038cf9205024c2dcdf58c96
hxf <hxf@892518100.com>
2019-09-19 17:16:30 +0800

    左侧导航栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>我是主页</div>
+<!-- 大容器 -->
+    <el-container>
+<!-- 左侧导航 -->
+<el-aside>
+    <layout-aside></layout-aside>
+  </el-aside>
+<!-- 右侧大容器 -->
+    <el-container>
+      <!-- 顶部导航 -->
+      <el-header>顶部导航</el-header>
+      <!-- 二级路由 -->
+      <el-main>容器</el-main>
+    </el-container>
+    </el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
